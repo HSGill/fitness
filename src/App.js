d29c6dc73f73821530d9fa4577d64b11ac6c61ba
HSGill <harjotgill73@gmail.com>
2019-09-30 20:58:46 +1300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Container">
+    <div className="item App-header">Workout Program</div>
+    <div className="item App-header">Meal Plan</div>
+  
+    </div>
+  );
+}
+
+export default App;
