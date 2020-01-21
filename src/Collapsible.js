cf9b72010861435d1fb1784b090f6655d7484596
HSGill <harjotgill73@gmail.com>
2020-01-21 20:41:00 +1300

    Stage-1 Completed

--- src/Collapsible.js
+++ src/Collapsible.js
@@ -1,44 +1,27 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Component } from 'react';
+import './Collapsible.css';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      width: '100%',
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-  }));
+class Collapsible extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      open: false
+    }
 
-export default function  Collapsible() {
-
-    const classes = useStyles();
-
-        return (
-            
-            <div className={classes.root}>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>Expansion Panel 1</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-            </div>
-        );
-    
+  }
+  togglePanel (){
+    this.setState({open : !this.state.open});
+  }
+  render(){
+    return(<div>
+      <div onClick= {(e)=> this.togglePanel(e)} className='header'>{this.props.title}</div>
+      {this.state.open ? (<div className='content'>
+             {this.props.children} </div>
+      ) : null }
+      </div>
+    );
+  }
 }
+
+export default Collapsible;
--- src/Workout.js
+++ src/Workout.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import'./Workout.css';
 import { Component } from 'react';
-import './Collapsible.js';
 import Collapsible from './Collapsible.js';
 
 class Workout extends Component {
+  
     render(){
         return (
             <div>
-            <Collapsible/>
+            <Collapsible title='Week1'> <Collapsible title='Day 1'>
+            <div> Exercise 1= Chest- Barbell Incline Bench Press-->Normal Set Reps: 15/12/10</div>
+             <div>Exercise 2= Chest-PUSH Up-->Normal Reps: 15/12/10</div>
+             <div>Exercise 3= Chest-Upper Pec Contractions-->Reps: 15/12/10</div>
+             <div>Exercise 4= Dumbbell Incline Flys--> Reps: 15/12/10</div>
+             <div>Exercise 5= Decline Sit Ups--->Reps 25/25</div>
+             <div>Exercise 6= Basic Crunches --->Reps 25/25</div>
+             <div>Exercise 7= Hanging Pikes --->Reps 25/25</div>
+             <div>Exercise 8= Frog Sit Ups --->Reps 25/25</div>
+             <div>Exercise 9= Snatch Shrug --->Reps 15/15/15(Trapezium)</div>
+             <div>Exercise 10= Barbell Shrugs(Behind Back) --->Reps 15/15/15(Trapezium) </div>
+             <div>Exercise 11= Standing Dumbbell Upright Row(Close Grip)--->Reps 15/15/15(Trapezium)</div>
+            </Collapsible>  
+            <Collapsible title='Day 2' ></Collapsible>
+            <Collapsible title='Day 3' ></Collapsible>
+            <Collapsible title='Day 4' ></Collapsible>
+            <Collapsible title='Day 5' ></Collapsible>
+            <Collapsible title='Day 6' ></Collapsible>
+            <Collapsible title='Day 7' ></Collapsible>
+            </Collapsible>
+            
             </div>
      
         );
